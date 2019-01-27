b1ec627221946e0ba5a2bb352da07786dbd500a2
Alex Cotoranu <alexcotoranu@gmail.com>
2019-01-27 18:20:07 +0100

    can now check for tasks in a test db

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,27 +1,26 @@
 var express = require('express'),
     router = express.Router(),
-    rethinkdb = require('rethinkdb'); //rethinkdb (js driver) connection
+    r = require('rethinkdb'); //rethinkdb (js driver) connection
     bodyParser = require('body-parser'), //parses information from POST
     methodOverride = require('method-override'); //used to manipulate POST
     multer = require('multer'); // used to upload files
 
-//RethinkDB Connection
 var connection = null;
-rethinkdb.connect( {host: 'localhost', port: 28015}, function(err, conn) {
+r.connect( {host: 'localhost', port: 28015}, function(err, conn) {
     if (err) throw err;
     connection = conn;
-})
+});
 
 //Any requests to this controller must pass through this 'use' function
 //Copy and pasted from method-override
 router.use(bodyParser.urlencoded({ extended: true }))
 router.use(methodOverride(function(req, res){
-        if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-        // look in urlencoded POST bodies and delete it
-        var method = req.body._method
-        delete req.body._method
-        return method
-        }
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    var method = req.body._method
+    delete req.body._method
+    return method
+    }
 }))
 
 //build the REST operations at the base for tasks
@@ -29,13 +28,46 @@ router.use(methodOverride(function(req, res){
 router.route('/')
     .get(function(req, res, next) {
         /* GET all tasks */
-        res.render('tasks/index', { title: 'All My Tasks' });
+        r.table('tasks').run(connection, function(err, cursor) {
+            if (err) throw err;
+            cursor.toArray(function(err, result) {
+                if (err) {
+                    throw err;
+                } else {
+                    //respond to both HTML and JSON. JSON responses require 'Accept: application/json;' in the Request Header
+                    // console.log(tasks);
+                    console.log(JSON.stringify(result, null, 2));
+                    var tasks = result;
+                    res.format({
+                        //HTML response will render the index.jade file in the views/tasks folder. We are also setting "tasks" to be an accessible variable in our jade view
+                        html: function(){
+                            res.render('tasks/index', {
+                                title: 'All my Tasks',
+                                "tasks" : tasks
+                            });
+                        },
+                        //JSON response will show all tasks in JSON format
+                        json: function(){
+                            res.json(infophotos);
+                        }
+                    });
+                }
+            });
+        });
+    })
+    .post(function(req, res) {
+        /* POST new task */
+        console.log(req.body); //should be all the text
+        console.log(req.files); //should be all the files
     });
 
-/* POST new tasks */
+/* GET new task form */
 router.get('/new', function(req, res) {
     res.render('tasks/new', { title: 'Add New Task' });
 });
+
+
+
 
 // route middleware to validate :id
 router.param('id', function(req, res, next, id) {
